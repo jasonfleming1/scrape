74ffe05801418371b44f11132aecba2dd1aa8984
jasonfleming1 <jason.fleming@comcast.net>
2019-05-31 19:40:43 -0500

    hbs loads, db fail

---
+++ public/assets/js/app.js
@@ -0,0 +1,73 @@
+//================ SCRAPE BUTTON ================
+
+$("#scrape").on("click", function() {
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data)
+        $(location).attr("href","/");
+    })
+}); // ==> end scrape button
+
+//================ SAVE ARTICLE BUTTON ================
+
+$(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save" + thisId
+    }).then(function(data) {
+        location.reload(); //load home page
+    })
+}); // ==> end save article button
+
+//================ DELETE ARTICLE BUTTON ================
+
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete" + thisId
+    }).then(function(data) {
+        location.reload(); //load home page
+    })
+    .catch(function (err){
+        console.log("Error on line 36 app.js" + err);
+    })
+});  // ==> end delete article button
+
+//================ SAVE NOTE BUTTON ================
+
+$(".saveNote").on("click", function() {
+    event.preventDefault();
+    var note = {};
+    id = $(this).attr("data-id");
+    note.body = $("#noteText").val().trim();
+    console.log("noteTest");
+    console.log("note: " + note.body);
+    if (!$("#noteText").val()) {
+        alert("Please enter a note to save your comment(s)!")
+    } else {
+        console.log("note id: " + id);
+        $.post("/notes/save" + id, { body: note.body }).then(function(data) {
+            $("noteText").val("");
+            $(".modalNote").modal("hide");
+            location.href = ("/saved"); //load "saved" page
+        });
+    }
+}); // ==> end save note
+
+//================ DELETE NOTE BUTTON ================
+
+$(".deleteNote").on("click", function() {
+    var noteId = $(this).attr("data-note-id");
+        $.ajax({
+            method: "GET",
+            url: "/notes/delete/" + noteId
+        }).done(function(data) {
+            $(".modalNote").modal("hide");
+            $(location).attr("href","/saved"); //load "saved" page
+        })
+
+}); // ==> end delete note
--- server.js
+++ server.js
@@ -35,8 +35,14 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 //use handlebars in the app
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main",
+      partialsDir: __dirname + '/views/partials/'
+    })
+  );
+  app.set("view engine", "handlebars");
 
 //================ DB CONNECTION {heroku required} ================
 
@@ -45,15 +51,66 @@ mongoose.connect(MONGODB_URI);
 
 //================ ROUTES ================
 
-//LOAD Page
-
-//GET scrape website
+//GET home route
+app.get("/", function (req, res) {
+    db.Article.find({ "saved": false }, function (error, data) {
+      var home = {
+        article: data
+      };
+      console.log(home);
+      res.render("index", home);
+    });
+  });// ==> get home
 
 //GET isSavedArticles
+app.get("/saved", function (req, res) {
+    db.Article.find({ "saved": true }).populate("note").exec(function (error, articles) {
+      var isSaved = {
+        article: articles
+      };
+      console.log(articles);
+      res.render("saved", isSaved);
+    });
+  });
+
+//GET scrape website
+app.get("/scrape", function(req, res) {
+
+    //axios request at the site
+    axios.get("https://www.dailynorseman.com/").then(function(res) {
+        
+        //let cheerio handle the response
+        var $ = cheerio.load(res.data);
+
+        //class that contains articles
+        $("l-hero").each(function (i, element) {
+
+            //empty result object
+            var result = {};
+
+            //get the data 
+            result.title = $(element).find("h2").text().trim();
+            //result.link = $(element).find("a").attr("href");
+            //result.summary = $(element).find("p").text().trim();
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+            }); // --> end article iterations
+
+            //success message
+            //res.send("Scrape Complete!");
+
+    });// ==>end axios.get
+}); // ==> end GET scrape
 
 //POST save article
 
-//GET delete article
+//GET remove from saved
 
 //POST create note
 
